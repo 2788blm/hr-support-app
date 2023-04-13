57fc15325d7caa8b2179dfed0fc8a2d3260bccca
2788blm <2711mm@gmail.com>
2023-04-14 00:02:48 +0200

    Initial commit

---
+++ src/main/java/intens/intensinterviewtask/IntensInterviewTaskApplication.java
@@ -0,0 +1,13 @@
+package intens.intensinterviewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensInterviewTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensInterviewTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/intens/intensinterviewtask/IntensInterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package intens.intensinterviewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensInterviewTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
